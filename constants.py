29c07713f0fc7251c476bd15586b5f9ad18c9ff2
Michael Shields <mr.m.p.shields@gmail.com>
2024-10-25 15:09:03 +0000

    Add constants.py

---
+++ constants.py
@@ -0,0 +1,22 @@
+"""
+Assigning all "magic strings"to constants, 
+to avoid typos and enable easier maintenance,
+in the event that the category names need to be changed. 
+"""
+
+# MAIN MENU OPTION NAMES
+INCOME_MENU = "Enter Income"
+EXPENSE_MENU = "Enter Expenses"
+BUDGET_MENU = "Display Budget"
+EXIT_MENU = "Exit Program"
+
+# FINANCES ATTRIBUTES NAMES
+INCOME = "Income"
+EXPENSES = "Expenses"
+ESSENTIAL = "Essential"
+NON_ESSENTIAL = "Non Essential"
+
+# EXPENSES ATTRIBUTE NAMES
+TITLE = "title"
+AMOUNT = "amount"
+CATEGORY = "category"
